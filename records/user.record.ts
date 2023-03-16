de08d86ffe827d4058391484c69db7d5d0f67d04
Hiob <hiob_93@o2.pl>
2023-03-16 10:00:05 +0100

    Fixes to the constructor and insert method.

--- records/user.record.ts
+++ records/user.record.ts
@@ -1,9 +1,22 @@
 import {pool} from "../utils/db";
 import {v4 as uuid} from "uuid";
 import {createHash} from "../utils/hash";
+import {UserEntity} from "../types/user/user.entity";
 
-class UserRecord {
-    private _id: string;
+
+export class UserRecord {
+    constructor(obj: UserEntity) {
+        this._login = obj.login;
+        this._name = obj.name;
+        this._password = obj.password;
+        (obj.id != undefined) ? this._id = obj.id : this._id = uuid();
+        (obj.created != undefined) ? this._created = obj.created : this._created = new Date();
+        if (obj.protein != undefined) this._protein = obj.protein;
+        if (obj.fat != undefined) this._fat = obj.fat;
+        if (obj.carbs != undefined) this._carbs = obj.carbs;
+        if (obj.kcal != undefined) this._kcal = obj.kcal;
+        if (obj.weight != undefined) this._weight = obj.weight;
+    }
     private _login: string;
     private _password: string;
     private _name: string;
@@ -14,41 +27,18 @@ class UserRecord {
     private _kcal?: number;
     private _weight?: [number];
 
-    constructor(obj: UserRecord) {
-        obj.id ? this._id = obj.id : this._id = uuid();
-        this._login = obj.login;
-        this._name = obj.name;
-        obj.created ? this._created = obj.created : this._created = new Date();
-        if (obj.protein) this._protein = obj.protein;
-        if (obj.fat) this._fat = obj.fat;
-        if (obj.carbs) this._carbs = obj.carbs;
-        if (obj.kcal) this._kcal = obj.kcal;
-        if (obj.weight) this._weight = obj.weight;
-    }
+    private _id?: string;
 
-    async insert(): Promise<string> {
-        try {
-            await pool.execute(
-                "INSERT INTO `users`(`id`, `login`, `password`, `name`, `created`) VALUES(:id, :login, :password, :name, :created)", {
-                    id: this._id,
-                    login: this._login,
-                    password: createHash(this._password, process.env.passwordSALT),
-                    name: this._name,
-                    created: this._created,
-                });
-            return this._id;
-        } catch (e) {
-            console.log(e)
-            throw new Error('Something gone wrong in function Insert for User');
-        }
-    }
-
-    get id(): string {
+    get id(): string | undefined {
         return this._id;
     }
 
-    set id(value: string) {
+    set id(value: string | undefined) {
         this._id = value;
+    }
+
+    get created(): Date | undefined {
+        return this._created;
     }
 
     get login(): string {
@@ -75,51 +65,68 @@ class UserRecord {
         this._name = value;
     }
 
-    get created(): Date {
-        return this._created;
-    }
-
-    set created(value: Date) {
+    set created(value: Date | undefined) {
         this._created = value;
     }
 
-    get protein(): number {
+    get protein(): number | undefined {
         return this._protein;
     }
 
-    set protein(value: number) {
+    set protein(value: number | undefined) {
         this._protein = value;
     }
 
-    get fat(): number {
+    get fat(): number | undefined {
         return this._fat;
     }
 
-    set fat(value: number) {
+    set fat(value: number | undefined) {
         this._fat = value;
     }
 
-    get carbs(): number {
+    get carbs(): number | undefined {
         return this._carbs;
     }
 
-    set carbs(value: number) {
+    set carbs(value: number | undefined) {
         this._carbs = value;
     }
 
-    get kcal(): number {
+    get kcal(): number | undefined {
         return this._kcal;
     }
 
-    set kcal(value: number) {
+    set kcal(value: number | undefined) {
         this._kcal = value;
     }
 
-    get weight(): [number] {
+    get weight(): [number] | undefined {
         return this._weight;
     }
 
-    set weight(value: [number]) {
+    set weight(value: [number] | undefined) {
         this._weight = value;
+    }
+
+    async insert(): Promise<string> {
+        try {
+            await pool.execute(
+                "INSERT INTO `users`(`id`, `login`, `password`, `name`, `created`) VALUES(:id, :login, :password, :name, :created)", {
+                    id: this._id,
+                    login: this._login,
+                    password: createHash(this._password, process.env.passwordSALT),
+                    name: this._name,
+                    created: this._created,
+                });
+            await pool.execute(
+                "INSERT INTO `user_details`(`userId`) VALUES(:userId)", {
+                    userId: this._id,
+                });
+            return this._id;
+        } catch (e) {
+            console.log(e)
+            throw new Error('Something gone wrong in function Insert for User');
+        }
     }
 }
